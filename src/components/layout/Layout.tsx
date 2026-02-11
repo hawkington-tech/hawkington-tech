050bd8fd1ec8e5d98bfbfb3cc89043f1b00dab98
carissab96 <bayscarissa706@gmail.com>
2026-02-11 09:20:14 -0600

    removed unused Layout.module.css import from Layout component

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,6 +1,6 @@
 import Header from './Header'
 import Footer from './Footer'
-import styles from './Layout.module.css'
+// import styles from './Layout.module.css'
 
 import { Outlet } from 'react-router-dom'
 
